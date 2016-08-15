addda3b589e393ce4435d5e98565f1e40c686248
empireofryan <rjohnson2001@gmail.com>
2016-08-14 20:11:26 -0400

    search added to song model and controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,10 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    if params[:search]
+      @songs = Song.search(params[:search]).order("created_at DESC")
+    else
+      @songs = Song.all.order('created_at DESC')
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ApplicationRecord
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+    where("content LIKE ?", "%#{search}%")
+  end
 end
